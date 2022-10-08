92c85f9b733000a3ae38a57919cba222c552bb15
Martin P <70173574+MartinP3@users.noreply.github.com>
2022-10-08 21:46:26 +0200

    Handlers for login and register

---
+++ src/js/handlers/register.mjs
@@ -0,0 +1,23 @@
+import { register } from "../api/auth/register.mjs";
+
+export function setRegisterFormListener()  {
+    const form = document.querySelector("#registerForm");
+
+    if (form) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const form = event.target;
+            const formData = new FormData(form);
+            const profile = Object.fromEntries(formData.entries())
+            if (profile.banner === "") {
+                delete profile.banner;
+            }
+            if (profile.avatar === "") {
+                delete profile.avatar;
+            }
+    
+            // Send to API
+            register(profile);
+        })
+    }
+}
