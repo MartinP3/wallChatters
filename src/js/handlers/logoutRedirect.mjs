ccdc33620af62f71cc8d69e9fa602e299eb472a7
Martin P <70173574+MartinP3@users.noreply.github.com>
2022-10-15 15:46:42 +0200

    oops

---
+++ src/js/handlers/logoutRedirect.mjs
@@ -0,0 +1,7 @@
+// Redirects if not logged in
+// export async function loginCheck() {
+//     const name = localStorage.getItem("profile");
+//     if (!name) {
+//         location.href = "/profile/login";
+//     }
+// }
