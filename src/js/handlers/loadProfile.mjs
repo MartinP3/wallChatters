ecaeb8d555c3d2d2e247648ce9142e53bafd3357
Martin P <70173574+MartinP3@users.noreply.github.com>
2022-10-16 12:54:11 +0200

    fixed profile loading in

--- src/js/handlers/loadProfile.mjs
+++ src/js/handlers/loadProfile.mjs
@@ -1,10 +1,13 @@
 // Loads the profile in
+import { load } from "../storage/index.mjs";
 import * as templates from "../templates/index.mjs";
 
 // Adds profile content
 export async function addProfile({profile} = {}) {
-  if (profile.name) {
+  const userProfile = load("profile");
+
+  if (userProfile.name) {
       const container = document.querySelector("#profile");
-      templates.renderProfileTemplate(profile, container);
+      templates.renderProfileTemplate(userProfile, container);
   }
 }
